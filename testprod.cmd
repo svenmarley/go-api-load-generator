f608ea9b4c513f989eb15cbbf148eaf542f5abe7
Mike Anderson <mikea@candicontrols.com>
2021-03-21 14:35:22 -0700

    Minor tweaks to documentation and the volumes of workers and children.

--- testprod.cmd
+++ testprod.cmd
@@ -1,3 +1,3 @@
-go run ./main.go -host_name=https://c1i55mxsd6.execute-api.us-west-2.amazonaws.com -host_path=/Live -authorization=X-Api-Key:RIqhxTAKNGaSw2waOY2CW3LhLny2EpI27i56VA6N -num_workers=150 -num_tries=100 -desired_rps=5
+go run ./main.go -host_name=https://c1i55mxsd6.execute-api.us-west-2.amazonaws.com -host_path=/Live -authorization=X-Api-Key:RIqhxTAKNGaSw2waOY2CW3LhLny2EpI27i56VA6N -num_workers=100 -num_tries=100 -desired_rps=5
 
 

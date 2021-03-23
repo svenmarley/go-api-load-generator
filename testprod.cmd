e5f6796bf7b412a79682ce47feac7de6924e1a0f
Mike Anderson <mikea@candicontrols.com>
2021-03-23 13:36:21 -0700

    Renamed the main file to go-api-load-generator.go

--- testprod.cmd
+++ testprod.cmd
@@ -1,3 +1,3 @@
-go run ./main.go -host_name=https://c1i55mxsd6.execute-api.us-west-2.amazonaws.com -host_path=/Live -authorization=X-Api-Key:RIqhxTAKNGaSw2waOY2CW3LhLny2EpI27i56VA6N -name=Bill -num_workers=30 -desired_rps=300  
+go run ./go-api-load-generator.go -host_name=https://c1i55mxsd6.execute-api.us-west-2.amazonaws.com -host_path=/Live -authorization=X-Api-Key:RIqhxTAKNGaSw2waOY2CW3LhLny2EpI27i56VA6N -name=Bill -num_workers=30 -desired_rps=300
 
 
